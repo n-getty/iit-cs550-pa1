f6b78afe64865070684f3cc9f7a077c0dc42f374
Neil <ngett2@uis.edu>
2017-01-13 17:16:03 -0600

    Hello world class added as a java refresher

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+package main.java;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        HelloWorld hw = new HelloWorld();
+        hw.printHello();
+    }
+
+    private void printHello(){
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
