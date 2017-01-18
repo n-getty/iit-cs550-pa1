3e511d2fdc4b96030798570a5c2f009f80aec258
Neil <ngett2@uis.edu>
2017-01-18 17:11:45 -0600

    Organized packages

---
+++ src/main/java/peer/Hello.java
@@ -0,0 +1,7 @@
+package main.java.peer;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Hello extends Remote {
+    String sayHello(String name) throws RemoteException;
+}
---
+++ src/main/java/peer/Server.java
@@ -0,0 +1,30 @@
+package main.java.peer;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Server implements Hello {
+
+    public Server() {}
+
+    public String sayHello(String name) {
+	return "Hello, world:".concat(name).concat("!");
+    }
+
+    public static void main(String args[]) {
+
+	try {
+	    Server obj = new Server();
+	    Hello stub = (Hello) UnicastRemoteObject.exportObject(obj, 0);
+
+	    // Bind the remote object's stub in the registry
+	    Registry registry = LocateRegistry.getRegistry();
+	    registry.bind("Hello", stub);
+
+	    System.err.println("Server ready");
+	} catch (Exception e) {
+	    System.err.println("Server exception: " + e.toString());
+	    e.printStackTrace();
+	}
+    }
+}
