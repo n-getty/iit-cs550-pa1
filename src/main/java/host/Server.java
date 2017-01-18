b249a7d82c98369dd1efdb650ff7840b841a5778
Neil <ngett2@uis.edu>
2017-01-18 17:57:33 -0600

    Basic methods for client-central server communication

--- src/main/java/host/Server.java
+++ src/main/java/host/Server.java
@@ -1,8 +1,8 @@
 package main.java.host;
-import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import static java.util.Arrays.asList;
@@ -11,7 +11,21 @@ public class Server implements IndexInt {
 
 	Map<String, List<Integer>> peerMap;
 
-    public Server() {}
+    public Server() {
+    	peerMap = new HashMap<String, List<Integer>>();
+		try {
+			IndexInt stub = (IndexInt) UnicastRemoteObject.exportObject(this, 0);
+
+			// Bind the remote object's stub in the registry
+			Registry registry = LocateRegistry.getRegistry();
+			registry.bind("PeerInt", stub);
+
+			System.err.println("Server ready");
+		} catch (Exception e) {
+			System.err.println("Server exception: " + e.toString());
+			e.printStackTrace();
+		}
+	}
 
 	public void register(int peerId, String fileName){
 		if(peerMap.containsValue(fileName)){
@@ -25,21 +39,4 @@ public class Server implements IndexInt {
 	public List<Integer> lookup(String fileName) {
 		return peerMap.get(fileName);
 	}
-
-    public static void main(String args[]) {
-
-	try {
-	    Server obj = new Server();
-		IndexInt stub = (IndexInt) UnicastRemoteObject.exportObject(obj, 0);
-
-	    // Bind the remote object's stub in the registry
-	    Registry registry = LocateRegistry.getRegistry();
-	    registry.bind("PeerInt", stub);
-
-	    System.err.println("Server ready");
-	} catch (Exception e) {
-	    System.err.println("Server exception: " + e.toString());
-	    e.printStackTrace();
-	}
-    }
 }
--- src/main/java/peer/Client.java
+++ src/main/java/peer/Client.java
@@ -1,23 +1,57 @@
 package main.java.peer;
 
+import main.java.host.IndexInt;
+
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Client {
+	PeerInt peerStub;
+	IndexInt indexStub;
+	int id;
+	List<String> fileList = new ArrayList<String>();
 
-    private Client() {}
-
-    public static void main(String[] args) {
-	String name = "Chris";
-	String host = (args.length < 1) ? null : args[0];
-	try {
-	    Registry registry = LocateRegistry.getRegistry(host);
-	    PeerInt stub = (PeerInt) registry.lookup("PeerInt");
-	    String response = stub.sayHello(name);
-	    System.out.println("response: " + response);
-	} catch (Exception e) {
-	    System.err.println("Client exception: " + e.toString());
-	    e.printStackTrace();
+    private Client(String host) {
+		try {
+			Registry registry = LocateRegistry.getRegistry(host);
+			//peerStub = (PeerInt) registry.lookup("PeerInt");
+			indexStub = (IndexInt) registry.lookup("IndexInt");
+		} catch (Exception e) {
+			System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+		}
 	}
-    }
+
+    public void register(String fileName){
+		fileList.add(fileName);
+		try {
+			indexStub.register(id, fileName);
+		} catch (Exception e) {
+			System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+		}
+	}
+
+	public void registerAll(){
+		try {
+			for(String fileName : fileList)
+				indexStub.register(id, fileName);
+		} catch (Exception e) {
+			System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+		}
+	}
+
+	public List<Integer> lookup(String fileName){
+		List targetIds = null;
+		try {
+			targetIds = indexStub.lookup(fileName);
+		} catch (Exception e) {
+			System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+		}
+		return targetIds;
+	}
 }
