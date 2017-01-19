83f756a77dd9b16b36e9ec2bbe8ded5335c45404
Neil <ngett2@uis.edu>
2017-01-18 18:43:19 -0600

    Rudimentary concurrent threading done in peer Server. Needs to be done for host server as well.

--- src/main/java/peer/Client.java
+++ src/main/java/peer/Client.java
@@ -6,6 +6,8 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Client {
 	PeerInt peerStub;
