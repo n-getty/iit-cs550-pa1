5bacfe6bde36e1d0bd1c2cf2a034c210f6972f3a
Neil <ngetty@hawk.iit.edu>
2017-01-19 15:49:30 -0600

    Skeleton classses

--- src/main/java/host/ServerDriver.java
+++
@@ -1,9 +0,0 @@
-package main.java.host;
-
-
-public class ServerDriver {
-    public static void main(String[] args) {
-        ServerImpl hostServer = new ServerImpl();
-
-    }
-}
--- src/main/java/peer/PeerDriver.java
+++ src/main/java/peer/PeerDriver.java
@@ -4,6 +4,7 @@ public class PeerDriver {
     public static void main(String[] args) {
         ClientImpl peerClient = new ClientImpl(args[0]);
         ServerImpl peerServer = new ServerImpl();
+        CommandLineParser clp = new DefaultParser();
 
     }
 }
